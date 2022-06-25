6a29b91613c16ce731815a71be99a52c472588b5
root <root@CentOS7.x-StagDocker.1.78>
2022-06-26 02:23:42 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# python-library-quick-start
-Template for create python package
+# Python library quick start
+This is a minimal template for uploading your python packages to pypi.org.
+
+## To Use
+* First clone this project.
+```bash
+# Clone this repository
+https://github.com/defartsa23/python-library-quick-start.git
+# Go into the repository
+cd python-library-quick-start
+```
+* Rename the folder `your-package` to the package name you want. then place all your files into a folder. Open the `__init__.py` file with a text editor of your choice. In this file, you write nothing but the import statement which has the following schema:
+```python
+from .Filename import Classname
+```
+* Replace the information in the `setup.py` file with the matching content.
+* Upload your package to PyPi
+```sh
+python setup.py sdist
+twine upload dist/*
+```
---
+++ setup.py
@@ -0,0 +1,41 @@
+from distutils.core import setup
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
+    packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
+    version = '0.1',      # Start with a small number and increase it with every change you make
+    license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+    long_description=long_description,            # Give a long description about your library
+    long_description_content_type='text/markdown',
+    author = 'YOUR NAME',                   # Type in your name
+    author_email = 'your.email@domain.com',      # Type in your E-Mail
+    url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+    download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+    project_urls={
+        'Documentation': 'https://packaging.python.org/tutorials/distributing-packages/',
+        'Funding': 'https://donate.pypi.org',
+        'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/pypa/sampleproject/',
+        'Tracker': 'https://github.com/pypa/sampleproject/issues',
+    }, 
+    keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ], 'Programming Language :: Python :: 3.6',
+)
